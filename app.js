0c3b8469272ef75134157b4abe2eab1791600654
Davis <davisbravin20@gmail.com>
2022-03-29 09:30:46 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,79 +1,21 @@
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session');
-const Mongostore = require('connect-mongodb-session')(session)
 require('dotenv').config()
-
 const app = express();
-
 app.set('x-powered-by' , false)
 
-// Passport Config
-require('./config/passport')(passport);
-
-// DB Config
-// const db = 'mongodb://localhost:27017/NETDATA';
-const db = process.env.DB_URI
-
-
-const store = new Mongostore({
-  uri : db,
-  collection : 'mySession'
-})
-
-
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true },
-    { useUnifiedTopoly: true }
-  )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
 
 // EJS
-// app.use(expressLayouts);
+
 app.set('view engine', 'ejs');
 
-// Express body parser
-app.use(express.urlencoded({ extended: true }));
-// app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-// Express session
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true,
-    store : store
-  })
-);
-
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Connect flash
-app.use(flash());
 
 
-// Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
-});
 
 // Routes
 app.use('/', require('./routes/router.js'));
-app.use('/users', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 3000;
 
--- routes/router.js
+++ routes/router.js
@@ -1,35 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const Schema = require('../models/schema')
-const {text , message  , posts , landingpage , login , register , admin , home , form , welcome} = require('../controllers/controller')
-
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
-
-// Welcome Page
 
 
-// Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('admin', {
-    user: req.user
-  })
-);
 
 
-router.get('/index' , text )
-router.get('/landingpage' , landingpage )
-router.get('/message' , message)
-router.post('/data' , posts)
-router.get('/login' , login)
-router.get('/register' , register)
-router.get('/welcome' , welcome)
 
+router.get('/landingpage' ,  (req , res)=>{
+    res.render('landing_page')
+})
 
-// router.get('/admin' ,   admin)
-router.get('/form' , form)
-
-
-router.get('/home' , home)
-
-
-module.exports = router;
+module.exports = router
